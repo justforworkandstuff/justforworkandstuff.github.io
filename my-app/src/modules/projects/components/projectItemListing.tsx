7e3b177bdaee0616747a7ed1b7d029f2cd62e701
Vitti Ng <justforworkandstuff@hotmail.com>
2025-03-19 18:16:36 +0800

    fix: mobile view content cropped off issue

--- my-app/src/modules/projects/components/projectItemListing.tsx
+++ my-app/src/modules/projects/components/projectItemListing.tsx
@@ -16,7 +16,7 @@ const ProjectItemListing = (props: ProjectItemListingProps) => {
       initial={{ opacity: 0, y: -20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5, delay: 0.2, ease: "easeIn" }}
-      className={`w-full h-full flex flex-col items-start md:items-end justify-start text-right text-lg md:text-sm leading-6 md:leading-4 md:max-h-[70vh] overflow-y-auto scrollbar-hide ${
+      className={`w-full h-full flex flex-col items-start md:items-end justify-start text-right text-lg md:text-sm leading-5 md:leading-4 md:max-h-[70vh] overflow-y-auto scrollbar-hide ${
         isThemeLight ? "text-black" : "text-gray-300"
       }`}
     >
